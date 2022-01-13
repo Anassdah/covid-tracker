307e901c4ace895b551edb4fb5b97d7b2bca199a
Anass Dahchour <74378330+Anassdah@users.noreply.github.com>
2022-01-13 12:13:01 +0100

    initial map commit

--- src/Map.js
+++ src/Map.js
@@ -1,19 +1,20 @@
-import React from 'react'
-import {MapContainer,TileLayer} from 'react-leaflet';
+import React from "react";
+import { Map as LeafletMap, TileLayer } from "react-leaflet";
 import "./Map.css";
-function Map() {
-    return (
-        <div className="map">
-            <MapContainer center={[51.0505,-0.09]} zoom={3} scrollWheelZoom={false}>
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                />
-            </MapContainer>
+import { showDataOnMap } from "./util";
 
-            
-        </div>
-    )
+function Map({ countries, casesType, center, zoom }) {
+  return (
+    <div className="map">
+      <LeafletMap center={center} zoom={zoom}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {showDataOnMap(countries, casesType)}
+      </LeafletMap>
+    </div>
+  );
 }
 
-export default Map
+export default Map;
